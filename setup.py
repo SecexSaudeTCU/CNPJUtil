4b3f245fa6b4d88d86219a6f177f72cfc68e8ef8
Monique <moniquelouise@gmail.com>
2020-11-20 16:27:00 -0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='cnpjutil',
+    version='1.0',
+    description='Módulo responsável por atuar como camada de acesso a dados de CNPJ, corporativos ou abertos',
+    author='Monique Monteiro',
+    author_email='moniquebm@tcu.gov.br',
+    packages=['cnpjutil','cnpjutil.cnpj'],
+    install_requires=['certifi', 'chardet', 'idna', 'pyodbc', 'requests', 'Unidecode', 'urllib3', 'wincertstore'],
+)
